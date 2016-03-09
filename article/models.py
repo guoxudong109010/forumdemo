f1dd6fc13cc7d43c347de18f00e4b24754b86321
guoxudong <627853570@qq.com>
2016-03-09 05:57:45 -0800

    3.9 commit

---
+++ article/models.py
@@ -0,0 +1,25 @@
+
+#coding:utf-8
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class Article(models.Model):
+    block=models.ForeignKey(Block,verbose_name=u"所属板块")
+    owner=models.ForeignKey(User,verbose_name=u"作者")
+    title=models.CharField(u"标题",max_length=300)
+    content=models.CharField(u"内容",max_length=30000)
+    status=models.IntegerField(u"状态",choices=((0,u"普通"),（-1,u"删除"）,(10,u"精华")),default=0)
+    create_timestamp=models.DateTimeField(auto_now_add=True)
+    last_update_timestamp=models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
+    class Meta:
+        verbose_name=u"文章"
+        verbose_name_plural=u"文章"
+
+    
---
+++ article/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response
+from block.models import Block
+from models import Article
+
+# Create your views here.
+
+def article_list(request,block_id):
+    block_id=int(block_id)
+    block=Block.objects.get(id=block_id)
+    articles=Article.objects.filter(block=block).order_by("-last_update_timestamp")
+    return render_to_response("article_list.html", {"articles":articles,"block":block})
+
