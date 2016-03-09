2165d63cbabe70ad59242efec3fe28927223814f
guoxudong <627853570@qq.com>
2016-03-09 06:11:27 -0800

    3.9.22:10 commit

--- article/models.py
+++ article/models.py
@@ -12,7 +12,7 @@ class Article(models.Model):
     owner=models.ForeignKey(User,verbose_name=u"作者")
     title=models.CharField(u"标题",max_length=300)
     content=models.CharField(u"内容",max_length=30000)
-    status=models.IntegerField(u"状态",choices=((0,u"普通"),（-1,u"删除"）,(10,u"精华")),default=0)
+    status=models.IntegerField(u"状态",choices=((0,u"普通"),(-1,u"删除"),(10,u"精华")),default=0)
     create_timestamp=models.DateTimeField(auto_now_add=True)
     last_update_timestamp=models.DateTimeField(auto_now=True)
 
