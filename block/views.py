eb54b67a9df89529ee926c2f4ff6c99faea2729b
guoxudong <627853570@qq.com>
2016-04-01 02:31:41 -0700

    4.1.17.26 commit

--- block/views.py
+++ block/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
 from models import Block
+from usercenter.models import ActivateCode
 
 # Create your views here.
 
 def block_list(request):
     blocks=Block.objects.all().order_by("-id")
-    return render_to_response("block_list.html", {"blocks":blocks})
+    return render_to_response("block_list.html", {"blocks":blocks},context_instance=RequestContext(request))
+        return redirect(reverse("login"))
